8f84e85bf8f0db3d66c4c83a22cf41ab1797942a
jessicanjos <jessicanjos2003@gmail.com>
2022-08-25 21:59:07 -0300

    app com caixas de textos

---
+++ app/src/main/java/br/com/etecia/petslove/TextActivity.java
@@ -0,0 +1,14 @@
+package br.com.etecia.petslove;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TextActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.text_layout);
+    }
+}
